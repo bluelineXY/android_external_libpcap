2350def5cc7a266fc6eb9654fa4bc044a5d299e7
bluelineXY <m.schroen@web.de>
2014-01-17 19:42:07 +0100

    Add sharedlib build

--- Android.mk
+++ Android.mk
@@ -24,3 +24,29 @@ LOCAL_CFLAGS+=-DHAVE_CONFIG_H -D_U_="__attribute__((unused))" -Dlinux -D__GLIBC_
 LOCAL_MODULE:= libpcap
 
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=\
+        bpf_dump.c\
+        bpf/net/bpf_filter.c\
+        bpf_image.c\
+        etherent.c\
+        fad-gifc.c\
+        gencode.c\
+        grammar.c\
+        inet.c\
+        nametoaddr.c\
+        optimize.c\
+        pcap.c\
+        pcap-linux.c\
+        savefile.c\
+        scanner.c\
+        version.c
+
+LOCAL_CFLAGS:=-O2 -g
+LOCAL_CFLAGS+=-DHAVE_CONFIG_H -D_U_="__attribute__((unused))" -Dlinux -D__GLIBC__ -D_GNU_SOURCE
+
+LOCAL_MODULE:= libpcap
+
+include $(BUILD_SHARED_LIBRARY)
